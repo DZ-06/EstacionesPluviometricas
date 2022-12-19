b0d27a3e6eba02511e11336e9917c13a2649dc52
DZ-06 <dsenamhi@gmail.com>
2022-12-18 21:13:23 -0500

    agregando tabla a marker

--- mapa.js
+++ mapa.js
@@ -13,7 +13,42 @@ document.getElementById('select-location').addEventListener('change',function(e)
 //marcador
 L.marker([-16.41350,-71.53438]).addTo(map).bindPopup(`
     <h5>La Pampilla</h5>
-    <p>PP=0.0</p>
+    
+    <div class="row mt-2">
+      <div class="col-12">
+        <table class="table table-striped text-center">
+          <thead>
+            <tr>
+              <th>Parámetro</th>
+              <th>Valor</th>
+              <th>Fecha</th>
+              <th>Hora</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>PP-10min</td>
+              <td>0.0</td>
+              <td>19-12-2022</td>
+              <td>07:00</td>
+            </tr>
+            <tr>
+                <td>PP-1h</td>
+                <td>0.0</td>
+                <td>19-12-2022</td>
+                <td>07:00</td>
+            </tr>
+            <tr>
+                <td>PP-10min</td>
+                <td>0.0</td>
+                <td>19-12-2022</td>
+                <td>07:00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
 `);
 L.marker([-16.488200,-71.333800]).addTo(map).bindPopup(`
     <h5>Tuctumpaya</h5>
